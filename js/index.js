56f7191d4392f4553ec5089f91481f9326308670
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-07-08 21:35:38 -0700

    added mouseover, keydown, load event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // Your code goes here
+const bodyColor = document.querySelector('body')
+
+bodyColor.addEventListener('click', function(event){
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color ='white'
+})
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+        document.body.style.backgroundColor = 'yellow'
+        document.body.style.color = 'black'
+    }
+})
+
+
+let navAnchors = document.querySelectorAll('.nav')[0]
+console.log(navAnchors)
+
+navAnchors.addEventListener('mouseover', function(event) {
+    event.target.style.color = "white";
+    setTimeout(function() {
+        event.target.style.color = 'black';
+    }, 500)
+})
+
+let h1 = document.querySelector('h1')
+
+h1.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'orange'
+    event.target.style.fontSize = '50px'
+})
+
+let h2 = document.querySelector('h2')
+h2.addEventListener('mouseleave', function(event){
+    event.target.style.fontSize = '50px'
+    event.target.style.color = 'orange'
+})
+
+let h2LetsGo = document.querySelectorAll('h2')[1]
+h2LetsGo.addEventListener('mouseleave', function(event){
+    event.target.style.fontSize = '50px'
+    event.target.style.color = 'orange'
+})
+
+let pContent = document.querySelector('p')
+console.log(pContent)
+
+pContent.addEventListener('keydown', function(event) {
+    event.target.style.fontSize = '50px'
+    
+})
+
+window.addEventListener('load', function(event) {
+    this.alert('Loaded!')
+})
